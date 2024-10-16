53765a30e1525ca841d15ad23466ab0b93177a3f
Bayhaqiindra <bayhaqi30072004.com>
2024-10-16 22:39:37 +0700

    Spacer adalah digunakan untuk menambahkan ruang kosong di antara elemen-elemen di layout, lalu modifier digunakan untuk menentukan bagaimana spasi ini bertindak. Dalam hal ini, Modifier.height(16.dp) berarti tinggi dari spasi yang dibuat adalah 16 dp

--- app/src/main/java/com/example/questbasiccomposable_106/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_106/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,6 +20,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.questbasiccomposable_106.ui.theme.QuestBasicComposable_106Theme
 
@@ -59,6 +62,8 @@ fun Asnawi(modifier: Modifier = Modifier) {
                 fontFamily = FontFamily.Monospace
             ),
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
 
