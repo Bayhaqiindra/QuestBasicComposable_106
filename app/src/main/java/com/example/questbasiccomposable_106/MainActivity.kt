e4ff4e31e68191ed8ef5c66742e40c842196ee22
Bayhaqiindra <bayhaqi30072004.com>
2024-10-16 22:50:24 +0700

    Fungsi ini memungkinkan Anda untuk menampilkan gambar di layar

--- app/src/main/java/com/example/questbasiccomposable_106/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_106/MainActivity.kt
@@ -1,21 +1,25 @@
 package com.example.questbasiccomposable_106
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -64,6 +68,10 @@ fun Asnawi(modifier: Modifier = Modifier) {
         )
 
         Spacer(modifier = Modifier.height(16.dp))
+
+        Image (
+
+        )
     }
 }
 
